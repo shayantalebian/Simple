4c2d229bc02184dd5d89a407d9a274d86f8efcda
Shayan Talebian <shayantalebianwork@gmail.com>
2021-03-02 15:35:52 +0330

    Third commit
    Fix a small bug.

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ function shayan()
 {
     alert(Date())
     document.getElementById("div1").style.width = "50%"
-    document.getElementById("dive1").style.height = "50%"
+    document.getElementById("div1").style.height = "50%"
 }
